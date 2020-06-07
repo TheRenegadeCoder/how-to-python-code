21b5b3a1b061deacef5ee61b248a78b80084a2d4
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-06-07 18:01:31 -0400

    Added my floor and ceiling solution

---
+++ challenges/how-to-round-a-number-in-python/floor_and_ceiling.py
@@ -0,0 +1,13 @@
+import math
+
+# Floor (round to -∞)
+math.floor(1.2)  # 1
+math.floor(1.5)  # 1
+math.floor(1.7)  # 1
+math.floor(-1.4)  # -2
+
+# Cieling (round to +∞)
+math.ceil(1.2)  # 2
+math.ceil(1.5)  # 2
+math.ceil(1.7)  # 2
+math.ceil(-1.4)  # -1
