3159e9d7ec0f624910a50dde9fc1ce83ce26d881
Al Muhimen <44261822+Muhimen123@users.noreply.github.com>
2020-07-08 15:11:57 +0600

    updated open.py
    Although the previous code worked just perfectly, I think it's often a good idea to follow the best practices.

--- challenges/how-to-open-file/with_open.py
+++ challenges/how-to-open-file/with_open.py
@@ -1,3 +1,7 @@
 path = input("Please enter the path to a text file: ")
-with open(path) as my_file:
+with open(path, 'r') as my_file: # 'r' means we are opening the file in read mode
 	print(my_file.read())
+
+# Few other modes:
+# 'w' : We will open the the file in "write" mode. It will rewrite the previously written data.
+# 'a' : We will open the file in "append" mode. By this, we will write the data at the end of the file without rewriting.
