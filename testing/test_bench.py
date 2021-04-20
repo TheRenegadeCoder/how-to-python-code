399a329ee36ec9a53fce84bc8170e02accb8a29b
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-04-20 16:56:05 -0400

    Refined solution to move control to function defs

--- testing/test_bench.py
+++ testing/test_bench.py
@@ -2,24 +2,24 @@ import timeit
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+import copy
 
-def test_bench(funcs: list, test_data: list):
-    def control(*args): pass
-    funcs.insert(0, control)
+def test_bench(funcs: list, test_data: dict):
     results = _test_performance(funcs, test_data)
     _show_results(results)
 
-def _test_performance(funcs: list, test_data: list) -> pd.DataFrame:
+def _test_performance(funcs: list, test_data: dict) -> pd.DataFrame:
     num_tests = len(funcs) * len(test_data)
     print(f"> Collecting {num_tests} test(s)")
     results = []
     i = 0
-    for test in test_data:
+    for name, test in test_data.items():
         for func in funcs:
             print(f"\t> Test Progress: {i + 1} / {num_tests}")
             print(f"\t\t> Function Name: {func.__name__}")
-            performance = min(timeit.repeat(lambda: func(test.copy())))
-            results.append([func.__name__, f"Test #{i // len(funcs) + 1}", performance])
+            print(f"\t\t> Test Name: {name}")
+            performance = min(timeit.repeat(lambda: func(*test)))
+            results.append([func.__name__, name, performance])
             i += 1
     print(f"> Testing Complete")
     return pd.DataFrame(results, columns=["Function", "Input", "Performance"])
