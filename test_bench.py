72ac0d5be3e6f167b0d0c9661e47e971edc93ffd
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-04-19 23:40:41 -0400

    Added plotting

--- test_bench.py
+++ test_bench.py
@@ -1,27 +1,27 @@
 import timeit
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def test_bench(funcs, test_data):
+    results = _test_performance(funcs, test_data)
+    _show_results(results)
+
+def _test_performance(funcs, test_data):
     num_tests = len(funcs) * len(test_data)
     print(f"> Collecting {num_tests} test(s)")
-    results = {}
+    results = []
     i = 1
     for test in test_data:
-        results[test] = {}
         for func in funcs:
             performance = min(timeit.repeat(lambda: func(test)))
-            results[test][func.__name__] = performance
+            results.append([func.__name__, test, performance])
             print(f"\t> Test Progress: {i} / {num_tests}")
             i += 1
     print(f"> Testing Complete")
-    return results
+    return pd.DataFrame(results, columns=["Function", "Input", "Performance"])
 
-def show_results(results):
-    print("> Displaying Results")
-    for test in results:
-        print(f"\t> Input: {test}")
-        for func in results[test]:
-            print(f"\t\t> {func}: {results[test][func]}")
-    print("> Results Displayed")
-
-results = test_bench([min, max], [(1, 2, 3), (-1, 4, 13)])
-show_results(results)
+def _show_results(results):
+    print(results.to_string())
+    sns.catplot(x="Input", y="Performance", hue="Function", kind="bar", data=pd.DataFrame(results))
+    plt.show()
