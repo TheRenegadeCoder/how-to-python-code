53ba18bf1f09a1818472e820f16c1e5befd8fd55
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-04-19 21:59:59 -0400

    Created a data structure for storing test results

--- test_bench.py
+++ test_bench.py
@@ -1,11 +1,27 @@
 import timeit
 
 def test_bench(funcs, test_data):
+    num_tests = len(funcs) * len(test_data)
+    print(f"> Collecting {num_tests} test(s)")
     results = {}
+    i = 1
     for test in test_data:
-        print(f"Current input: {test}")
+        results[test] = {}
         for func in funcs:
             performance = min(timeit.repeat(lambda: func(test)))
-            print(f"\t{func.__name__}: {performance}")
+            results[test][func.__name__] = performance
+            print(f"\t> Test Progress: {i} / {num_tests}")
+            i += 1
+    print(f"> Testing Complete")
+    return results
 
-test_bench([min], [(1, 2, 3)])
+def show_results(results):
+    print("> Displaying Results")
+    for test in results:
+        print(f"\t> Input: {test}")
+        for func in results[test]:
+            print(f"\t\t> {func}: {results[test][func]}")
+    print("> Results Displayed")
+
+results = test_bench([min, max], [(1, 2, 3), (-1, 4, 13)])
+show_results(results)
