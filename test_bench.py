fdeaaafb6433d8b118add965c9d6cb3563a15c9f
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-04-19 23:45:01 -0400

    Create a Test Bench to Commemorate 40 Articles in the Series (#33)
    * Created a basic test bench file
    
    * Added printing
    
    Next steps: return a data structure for plotting.
    
    * Created a data structure for storing test results
    
    * Added plotting
    
    * Added type hints

---
+++ test_bench.py
@@ -0,0 +1,27 @@
+import timeit
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def test_bench(funcs: list, test_data: list):
+    results = _test_performance(funcs, test_data)
+    _show_results(results)
+
+def _test_performance(funcs: list, test_data: list) -> pd.DataFrame:
+    num_tests = len(funcs) * len(test_data)
+    print(f"> Collecting {num_tests} test(s)")
+    results = []
+    i = 1
+    for test in test_data:
+        for func in funcs:
+            performance = min(timeit.repeat(lambda: func(test)))
+            results.append([func.__name__, test, performance])
+            print(f"\t> Test Progress: {i} / {num_tests}")
+            i += 1
+    print(f"> Testing Complete")
+    return pd.DataFrame(results, columns=["Function", "Input", "Performance"])
+
+def _show_results(results: pd.DataFrame):
+    print(results.to_string())
+    sns.catplot(x="Input", y="Performance", hue="Function", kind="bar", data=pd.DataFrame(results))
+    plt.show()
