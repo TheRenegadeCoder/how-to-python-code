15218b1f327ab4ce10925e641f3fd1ec2b2bd276
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-04-20 17:19:14 -0400

    Polished Test Bench for Scalability (#35)
    * Implemented prototype test bench for articles
    
    * Rename empty list script
    
    * Made beautiful plot
    
    * Adjusted legend font size
    
    * Fixed text scaling
    
    * Refined solution to move control to function defs
    
    * Added anime into lists

--- testing/empty_list.py
+++ testing/empty_list.py
@@ -1,26 +1,36 @@
 from test_bench import test_bench
 
+def control(anime):
+    anime = anime.copy()
+
 def empty_list_by_hand(anime):
+    anime = anime.copy()
     while anime:
         anime.pop()
 
 def empty_list_by_assignment(anime):
+    anime = anime.copy()
     anime = [] # Wouldn't actually work as a function
 
 def empty_list_by_clear(anime):
+    anime = anime.copy()
     anime.clear()
 
 def empty_list_by_del(anime):
+    anime = anime.copy()
     del anime[:]
 
 def empty_list_by_slice_assignment(anime):
+    anime = anime.copy()
     anime[:] = []
 
 def empty_list_by_multiplication(anime):
+    anime = anime.copy()
     anime *= 0  # Also, would not work as a function
 
 test_bench(
     [
+        control,
         empty_list_by_hand,
         empty_list_by_assignment,
         empty_list_by_clear,
@@ -28,10 +38,10 @@ test_bench(
         empty_list_by_slice_assignment,
         empty_list_by_multiplication
     ],
-    [
-        [],
-        [1],
-        [1, 3, 5],
-        [5] * 100
-    ]
+    {
+        "Empty List": [[]],
+        "One Item List": [["Your Lie in April"]],
+        "Small List": [["My Hero Academia", "Attack on Titan", "Steins;Gate"]],
+        "Large List": [["One Punch Man"] * 100]
+    }
 )
--- testing/test_bench.py
+++ testing/test_bench.py
@@ -2,27 +2,34 @@ import timeit
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+import copy
 
-def test_bench(funcs: list, test_data: list):
+def test_bench(funcs: list, test_data: dict):
     results = _test_performance(funcs, test_data)
     _show_results(results)
 
-def _test_performance(funcs: list, test_data: list) -> pd.DataFrame:
+def _test_performance(funcs: list, test_data: dict) -> pd.DataFrame:
     num_tests = len(funcs) * len(test_data)
     print(f"> Collecting {num_tests} test(s)")
     results = []
     i = 0
-    for test in test_data:
+    for name, test in test_data.items():
         for func in funcs:
             print(f"\t> Test Progress: {i + 1} / {num_tests}")
             print(f"\t\t> Function Name: {func.__name__}")
-            performance = min(timeit.repeat(lambda: func(test.copy())))
-            results.append([func.__name__, f"Test #{i // len(funcs) + 1}", performance])
+            print(f"\t\t> Test Name: {name}")
+            performance = min(timeit.repeat(lambda: func(*test)))
+            results.append([func.__name__, name, performance])
             i += 1
     print(f"> Testing Complete")
     return pd.DataFrame(results, columns=["Function", "Input", "Performance"])
 
 def _show_results(results: pd.DataFrame):
-    print(results.to_string())
-    sns.catplot(x="Input", y="Performance", hue="Function", kind="bar", data=pd.DataFrame(results))
+    print(results.to_string()) 
+    sns.set_theme()
+    with sns.plotting_context("paper", font_scale=1.5):
+        sns.catplot(x="Input", y="Performance", hue="Function", kind="bar", data=pd.DataFrame(results), legend=False, height=8, aspect=2)
+    plt.title("How to Python: Function Performance Comparison", fontsize=16)
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, title="Functions", fontsize='12', title_fontsize='12')
+    plt.tight_layout()
     plt.show()
