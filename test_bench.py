c1469403f912d0a7d7527796ed007b167a986865
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-04-19 23:44:48 -0400

    Added type hints

--- test_bench.py
+++ test_bench.py
@@ -3,11 +3,11 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def test_bench(funcs, test_data):
+def test_bench(funcs: list, test_data: list):
     results = _test_performance(funcs, test_data)
     _show_results(results)
 
-def _test_performance(funcs, test_data):
+def _test_performance(funcs: list, test_data: list) -> pd.DataFrame:
     num_tests = len(funcs) * len(test_data)
     print(f"> Collecting {num_tests} test(s)")
     results = []
@@ -21,7 +21,7 @@ def _test_performance(funcs, test_data):
     print(f"> Testing Complete")
     return pd.DataFrame(results, columns=["Function", "Input", "Performance"])
 
-def _show_results(results):
+def _show_results(results: pd.DataFrame):
     print(results.to_string())
     sns.catplot(x="Input", y="Performance", hue="Function", kind="bar", data=pd.DataFrame(results))
     plt.show()
