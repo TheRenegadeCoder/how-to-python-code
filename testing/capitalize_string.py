59907e8449a1758e603708488557d269aee2d1d6
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-06-09 14:53:17 -0400

    Added Capitalize Testing Code (#36)

---
+++ testing/capitalize_string.py
@@ -0,0 +1,42 @@
+from test_bench import test_bench
+
+def control(string):
+    pass
+
+def capitalize_by_hand(string):
+    character = string[0]
+    if 97 <= ord(character) <= 122:
+        shift = ord(character) - 32 
+        uppercase = chr(shift)
+        return uppercase + string[1:]
+    return string
+    
+def capitalize_by_mapping(string):
+    lowercase = "abcdefghijklmnopqrstuvwxyz"
+    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    character = string[0]
+    i = lowercase.find(character)
+    return string if i == -1 else uppercase[i] + string[1:]
+
+def capitalize_with_upper(string):
+    character = string[0]
+    return character.upper() + string[1:]
+
+def capitalize(string):
+    return string.capitalize()
+
+
+test_bench(
+    [
+        control,
+        capitalize_by_hand,
+        capitalize_by_mapping,
+        capitalize_with_upper,
+        capitalize
+    ],
+    {
+        "One Letter String": ["a"],
+        "Small String": ["how now brown cow"],
+        "Large String": ["One Punch Man" * 100]
+    }
+)
