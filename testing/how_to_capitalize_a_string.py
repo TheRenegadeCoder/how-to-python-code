d5ba99738ec68dcd42be12c15839d4ff8c16d2c7
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-06-19 18:32:48 -0400

    Reorganized file to include main

--- testing/how_to_capitalize_a_string.py
+++ testing/how_to_capitalize_a_string.py
@@ -7,11 +7,18 @@ https://therenegadecoder.com/code/how-to-capitalize-a-string-in-python/
 from test_bench import test_bench
 
 
-def control(_):
+def control(_) -> None:
+    """
+    Provides a control scenario for testing. In this case, none of the functions
+    share any overhead, so this function is empty.
+
+    :param _: a placeholder for the string input
+    :return: None
+    """
     pass
 
 
-def capitalize_by_hand(string):
+def capitalize_by_hand(string: str) -> str:
     character = string[0]
     if 97 <= ord(character) <= 122:
         shift = ord(character) - 32
@@ -20,7 +27,7 @@ def capitalize_by_hand(string):
     return string
 
 
-def capitalize_by_mapping(string):
+def capitalize_by_mapping(string: str) -> str:
     lowercase = "abcdefghijklmnopqrstuvwxyz"
     uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     character = string[0]
@@ -28,26 +35,31 @@ def capitalize_by_mapping(string):
     return string if i == -1 else uppercase[i] + string[1:]
 
 
-def capitalize_with_upper(string):
+def capitalize_with_upper(string: str) -> str:
     character = string[0]
     return character.upper() + string[1:]
 
 
-def capitalize(string):
+def capitalize(string: str) -> str:
     return string.capitalize()
 
 
-test_bench(
-    [
-        control,
-        capitalize_by_hand,
-        capitalize_by_mapping,
-        capitalize_with_upper,
-        capitalize
-    ],
-    {
-        "One Letter String": ["a"],
-        "Small String": ["how now brown cow"],
-        "Large String": ["One Punch Man" * 100]
-    }
-)
+def main():
+    test_bench(
+        [
+            control,
+            capitalize_by_hand,
+            capitalize_by_mapping,
+            capitalize_with_upper,
+            capitalize
+        ],
+        {
+            "One Letter String": ["a"],
+            "Small String": ["how now brown cow"],
+            "Large String": ["One Punch Man" * 100]
+        }
+    )
+
+
+if __name__ == '__main__':
+    main()
