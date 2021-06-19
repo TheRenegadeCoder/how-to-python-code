c0dce7552160b81bab2678c8d115f3947c97e988
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-06-19 17:48:34 -0400

    Added Testing Documentation (#51)
    * PEP8'd the Testing Code
    
    * Updated file formatting
    
    * Adding documentation to test bench
    
    * Added yet more documentation
    
    * Finished documenting the test bench

--- testing/how_to_empty_a_list.py
+++ testing/how_to_empty_a_list.py
@@ -5,7 +5,8 @@ https://therenegadecoder.com/code/how-to-empty-a-list-in-python/
 
 from test_bench import test_bench
 
-def control(anime: list(str)) -> None:
+
+def control(anime: list[str]) -> None:
     """
     Provides a control scenario for testing. In this case, all of the solutions
     rely on copying the input list, so the control function accounts for that.
@@ -14,7 +15,8 @@ def control(anime: list(str)) -> None:
     """
     anime = anime.copy()
 
-def empty_list_by_hand(anime: list(str)) -> None:
+
+def empty_list_by_hand(anime: list[str]) -> None:
     """
     Empties a list by repeatedly removing elements from a list.  
     
@@ -24,16 +26,18 @@ def empty_list_by_hand(anime: list(str)) -> None:
     while anime:
         anime.pop()
 
-def empty_list_by_assignment(anime: list(str)) -> None:
+
+def empty_list_by_assignment(anime: list[str]) -> None:
     """
     Empties a list by reassigning the reference.
     
     :param anime: a list of anime
     """
     anime = anime.copy()
-    anime = [] # Wouldn't actually work as a function
+    anime = []  # Wouldn't actually work as a function
 
-def empty_list_by_clear(anime: list(str)) -> None:
+
+def empty_list_by_clear(anime: list[str]) -> None:
     """
     Empties a list by calling the clear method.
     
@@ -42,7 +46,8 @@ def empty_list_by_clear(anime: list(str)) -> None:
     anime = anime.copy()
     anime.clear()
 
-def empty_list_by_del(anime: list(str)) -> None:
+
+def empty_list_by_del(anime: list[str]) -> None:
     """
     Empties a list by deleting a slice of the list.
     
@@ -51,7 +56,8 @@ def empty_list_by_del(anime: list(str)) -> None:
     anime = anime.copy()
     del anime[:]
 
-def empty_list_by_slice_assignment(anime: list(str)) -> None:
+
+def empty_list_by_slice_assignment(anime: list[str]) -> None:
     """
     Empties a list by replacing a slice of the list with an empty list.
     
@@ -60,7 +66,8 @@ def empty_list_by_slice_assignment(anime: list(str)) -> None:
     anime = anime.copy()
     anime[:] = []
 
-def empty_list_by_multiplication(anime: list(str)) -> None:
+
+def empty_list_by_multiplication(anime: list[str]) -> None:
     """
     Empties a list by multiplication.
     
@@ -68,6 +75,7 @@ def empty_list_by_multiplication(anime: list(str)) -> None:
     """
     anime = anime.copy()
     anime *= 0  # Also, would not work as a function
+
 
 def main() -> None:
     """
@@ -90,6 +98,7 @@ def main() -> None:
             "Large List": [["One Punch Man"] * 100]
         }
     )
-    
+
+
 if __name__ == "__main__":
     main()
--- testing/test_bench.py
+++ testing/test_bench.py
@@ -2,13 +2,35 @@ import timeit
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
-import copy
+
 
 def test_bench(funcs: list, test_data: dict):
+    """
+    Given a list of functions and a list of dictionary of test data,
+    this function will execute performance testing using all of the items
+    in the dictionary on all of the functions provided.
+
+    Dictionary must be in the form of the following:
+
+    {"test_1_name": ["list", "of", "arguments"]}
+
+    :param funcs: a list of functions
+    :param test_data: a dictionary of test data
+    """
     results = _test_performance(funcs, test_data)
     _show_results(results)
 
+
 def _test_performance(funcs: list, test_data: dict) -> pd.DataFrame:
+    """
+    Given a list of functions and a dictionary of test data, this function
+    creates a DataFrame containing the name of the function, the input to
+    that function, and the resulting performance of that combination. A row
+    is generated for all possible combinations of functions and test data items.
+
+    :param funcs: a list of functions
+    :param test_data: a dictionary of test data
+    """
     num_tests = len(funcs) * len(test_data)
     print(f"> Collecting {num_tests} test(s)")
     results = []
@@ -24,11 +46,27 @@ def _test_performance(funcs: list, test_data: dict) -> pd.DataFrame:
     print(f"> Testing Complete")
     return pd.DataFrame(results, columns=["Function", "Input", "Performance"])
 
+
 def _show_results(results: pd.DataFrame):
-    print(results.to_string()) 
+    """
+    Given a DataFrame of performance testing results, this function
+    plots the results in a figure. In addition, it dumps the results as a string.
+
+    :param results: a DataFrame containing the results of a performance test
+    """
+    print(results.to_string())
     sns.set_theme()
     with sns.plotting_context("paper", font_scale=1.5):
-        sns.catplot(x="Input", y="Performance", hue="Function", kind="bar", data=pd.DataFrame(results), legend=False, height=8, aspect=2)
+        sns.catplot(
+            x="Input",
+            y="Performance",
+            hue="Function",
+            kind="bar",
+            data=pd.DataFrame(results),
+            legend=False,
+            height=8,
+            aspect=2
+        )
     plt.title("How to Python: Function Performance Comparison", fontsize=16)
     plt.legend(bbox_to_anchor=(1.05, 1), loc=2, title="Functions", fontsize='12', title_fontsize='12')
     plt.tight_layout()
