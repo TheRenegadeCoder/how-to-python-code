3599402cc505fd0ead7c29bf0569a643be9727bb
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-06-19 18:00:05 -0400

    Added article link

--- testing/how_to_capitalize_a_string.py
+++ testing/how_to_capitalize_a_string.py
@@ -1,16 +1,25 @@
+"""
+Tests the performance of all of the solutions listed in the following article:
+https://therenegadecoder.com/code/how-to-capitalize-a-string-in-python/
+"""
+
+
 from test_bench import test_bench
 
-def control(string):
+
+def control(_):
     pass
+
 
 def capitalize_by_hand(string):
     character = string[0]
     if 97 <= ord(character) <= 122:
-        shift = ord(character) - 32 
+        shift = ord(character) - 32
         uppercase = chr(shift)
         return uppercase + string[1:]
     return string
-    
+
+
 def capitalize_by_mapping(string):
     lowercase = "abcdefghijklmnopqrstuvwxyz"
     uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -18,9 +27,11 @@ def capitalize_by_mapping(string):
     i = lowercase.find(character)
     return string if i == -1 else uppercase[i] + string[1:]
 
+
 def capitalize_with_upper(string):
     character = string[0]
     return character.upper() + string[1:]
+
 
 def capitalize(string):
     return string.capitalize()
