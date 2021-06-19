abe46cabed594092c8e2bc83f11d895911916bd6
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-06-19 17:24:54 -0400

    Fully documented testing

--- testing/how_to_empty_a_list.py
+++ testing/how_to_empty_a_list.py
@@ -16,7 +16,7 @@ def control(anime: list(str)) -> None:
 
 def empty_list_by_hand(anime: list(str)) -> None:
     """
-    Tests the performance of emptying a list by hand. 
+    Empties a list by repeatedly removing elements from a list.  
     
     :param anime: a list of anime
     """
@@ -24,40 +24,72 @@ def empty_list_by_hand(anime: list(str)) -> None:
     while anime:
         anime.pop()
 
-def empty_list_by_assignment(anime):
+def empty_list_by_assignment(anime: list(str)) -> None:
+    """
+    Empties a list by reassigning the reference.
+    
+    :param anime: a list of anime
+    """
     anime = anime.copy()
     anime = [] # Wouldn't actually work as a function
 
-def empty_list_by_clear(anime):
+def empty_list_by_clear(anime: list(str)) -> None:
+    """
+    Empties a list by calling the clear method.
+    
+    :param anime: a list of anime
+    """
     anime = anime.copy()
     anime.clear()
 
-def empty_list_by_del(anime):
+def empty_list_by_del(anime: list(str)) -> None:
+    """
+    Empties a list by deleting a slice of the list.
+    
+    :param anime: a list of anime
+    """
     anime = anime.copy()
     del anime[:]
 
-def empty_list_by_slice_assignment(anime):
+def empty_list_by_slice_assignment(anime: list(str)) -> None:
+    """
+    Empties a list by replacing a slice of the list with an empty list.
+    
+    :param anime: a list of anime
+    """
     anime = anime.copy()
     anime[:] = []
 
-def empty_list_by_multiplication(anime):
+def empty_list_by_multiplication(anime: list(str)) -> None:
+    """
+    Empties a list by multiplication.
+    
+    :param anime: a list of anime
+    """
     anime = anime.copy()
     anime *= 0  # Also, would not work as a function
 
-test_bench(
-    [
-        control,
-        empty_list_by_hand,
-        empty_list_by_assignment,
-        empty_list_by_clear,
-        empty_list_by_del,
-        empty_list_by_slice_assignment,
-        empty_list_by_multiplication
-    ],
-    {
-        "Empty List": [[]],
-        "One Item List": [["Your Lie in April"]],
-        "Small List": [["My Hero Academia", "Attack on Titan", "Steins;Gate"]],
-        "Large List": [["One Punch Man"] * 100]
-    }
-)
+def main() -> None:
+    """
+    Tests the performance of all the functions defined in this file. 
+    """
+    test_bench(
+        [
+            control,
+            empty_list_by_hand,
+            empty_list_by_assignment,
+            empty_list_by_clear,
+            empty_list_by_del,
+            empty_list_by_slice_assignment,
+            empty_list_by_multiplication
+        ],
+        {
+            "Empty List": [[]],
+            "One Item List": [["Your Lie in April"]],
+            "Small List": [["My Hero Academia", "Attack on Titan", "Steins;Gate"]],
+            "Large List": [["One Punch Man"] * 100]
+        }
+    )
+    
+if __name__ == "__main__":
+    main()
