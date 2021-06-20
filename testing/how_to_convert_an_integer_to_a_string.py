b7c4be2baed02f2e9c0b14fe5345e6312c628174
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-06-19 20:21:22 -0400

    Created basic test file

--- testing/how_to_convert_an_integer_to_a_string.py
+++ testing/how_to_convert_an_integer_to_a_string.py
@@ -4,3 +4,40 @@ https://therenegadecoder.com/code/how-to-convert-an-integer-to-a-string-in-pytho
 """
 
 from test_bench import test_bench
+
+
+def control(_):
+    pass
+
+
+def convert_by_type_casting(integer: int) -> str:
+    return str(integer)
+
+
+def convert_by_f_string(integer: int) -> str:
+    return f"{integer}"
+
+
+def convert_by_interpolation(integer: int) -> str:
+    return "%s" % integer
+
+
+def main():
+    test_bench(
+        [
+            control,
+            convert_by_type_casting,
+            convert_by_f_string,
+            convert_by_interpolation
+        ],
+        {
+            "Zero": [0],
+            "Single Digit": [5],
+            "Small Number": [1107321],
+            "Massive Number": [2 ** 64]
+        }
+    )
+
+    
+if __name__ == '__main__':
+    main()
