ce1568b6346692989bd3ad7fffd026ddb5a76427
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-06-20 16:32:33 -0400

    Reworking solutions files

--- challenges/how-to-empty-a-list/empty_list.py
+++
@@ -1,6 +0,0 @@
-def to_empty_or_not_to_empty(anime: list):
-    hasDuplicates = len(set(anime)) != len(anime)
-    hasSeventeenShows = len(anime) == 17
-    hasYourTitle = any("Your" in word for word in anime)
-    if hasDuplicates or hasSeventeenShows or hasYourTitle:
-        anime.clear()
