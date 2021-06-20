9bb3b1544c843c3ed20aa3fcdc53ffc01c484129
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-06-19 23:59:49 -0400

    Added challenge link

--- challenges/how-to-convert-an-integer-to-a-string/solutions.py
+++ challenges/how-to-convert-an-integer-to-a-string/solutions.py
@@ -1,4 +1,10 @@
-def reverse(num: int) -> str:
+"""
+Provides a series of solutions to the challenge provided at the following link:
+https://therenegadecoder.com/code/how-to-convert-an-integer-to-a-string-in-python/#challenge
+"""
+
+
+def reverse_1(num: int) -> str:
     output = ""
     while num != 0:
         output += num % 10
