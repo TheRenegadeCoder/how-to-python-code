cf19cc143527a069e8a846c52685c0389e729afb
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-06-19 23:53:18 -0400

    Reworked absolute value challenge

--- challenges/how-to-capitalize-a-string/solutions.py
+++ challenges/how-to-capitalize-a-string/solutions.py
@@ -5,4 +5,11 @@ https://therenegadecoder.com/code/how-to-capitalize-a-string-in-python/#challeng
 
 
 def capitalize_1(string):
+    """
+    Capitalizes a string using a combination of the upper and lower methods.
+
+    :author: jrg94
+    :param string: any string
+    :return: a string with the first character capitalized and the rest lowercased
+    """
     return string[0].upper() + string[1:].lower()
--- challenges/how-to-compute-absolute-value/compute-absolute-value-2.py
+++
@@ -1,9 +0,0 @@
-# Link to the challenge: https://therenegadecoder.com/code/how-to-compute-absolute-value-in-python/#challenge
-# This solution using simple string methods
-
-number = input("Enter a number >>> ")
-
-if '-' in number: 
-    print('-' * (-1 * int(number)))
-else:
-    print('+' * int(number))
