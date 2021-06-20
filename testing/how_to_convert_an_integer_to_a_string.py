b592feee5255b0fade840634ab5f55bf1f1d5845
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-06-19 20:16:48 -0400

    Created base file

---
+++ testing/how_to_convert_an_integer_to_a_string.py
@@ -0,0 +1,6 @@
+"""
+Tests the performance of all of the solutions listed in the following article:
+https://therenegadecoder.com/code/how-to-convert-an-integer-to-a-string-in-python/#performance
+"""
+
+from test_bench import test_bench
