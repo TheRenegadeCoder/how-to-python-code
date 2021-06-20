96f4d05318bc3e14de03e84b1b1ec02dd5328b68
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-06-19 23:57:20 -0400

    Added some test cases for the memes

--- challenges/how-to-capitalize-a-string/test_solutions.py
+++ challenges/how-to-capitalize-a-string/test_solutions.py
@@ -5,3 +5,13 @@ from inspect import getmembers, isfunction
 def test_capitalize_all_lowercase():
     for _, value in getmembers(solutions, isfunction):
         assert value("deku") == "Deku"
+
+
+def test_capitalize_no_alphabet():
+    for _, value in getmembers(solutions, isfunction):
+        assert value("1234") == "1234"
+
+
+def test_capitalize_all_caps():
+    for _, value in getmembers(solutions, isfunction):
+        assert value("TSUYU") == "Tsuyu"
