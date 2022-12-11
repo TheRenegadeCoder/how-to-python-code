5e8c7b8f6f97532ee3a39ec0be2db3f7b52ccbb8
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-12-11 14:51:27 -0500

    Added another test

--- testing/how_to_clamp_a_float.py
+++ testing/how_to_clamp_a_float.py
@@ -1,5 +1,6 @@
 from test_bench import test_bench
 
+
 def clamp_float_with_branching_nested(num: float, minimum: float, maximum: float):
     if num < minimum:
         return minimum
@@ -7,22 +8,27 @@ def clamp_float_with_branching_nested(num: float, minimum: float, maximum: float
         return maximum
     else:
         return num
-    
+
+
+def clamp_float_with_branching_flat(num: float, minimum: float, maximum: float):
+    return minimum if num < minimum else maximum if num > maximum else num
+
+
 def main() -> None:
     """
     Tests the performance of all the functions defined in this file. 
     """
     test_bench(
         [
-            clamp_float_with_branching_nested
+            clamp_float_with_branching_nested,
+            clamp_float_with_branching_flat
         ],
         {
-            "Lower Bound": [-.002, 0, .40]
+            "Lower Bound": [-.002, 0, .40],
+            "Upper Bound": [.402, 0, .40]
         }
     )
 
 
 if __name__ == "__main__":
     main()
-
-    
